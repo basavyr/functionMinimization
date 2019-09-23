624dd2f405cec9d93bd0c153490b0ae7a3731124
Robert Poenaru <robert.poenaru@protonmail.ch>
2019-09-23 16:06:06 +0300

    adds chrono library for time measurements

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <chrono>
+#include <omp.h>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+    auto sum = 0;
+    auto timeStart = chrono::high_resolution_clock::now();
+    for (int i = 1; i <= 1000000000; ++i)
+    {
+        sum += i * log(i);
+    }
+    auto timeStop = chrono::high_resolution_clock::now();
+    auto execTime = chrono::duration_cast<chrono::seconds>(timeStop - timeStart).count();
     cout << "FUNCTION OPTIMIZATION" << endl;
+    cout << "Execution time = " << execTime << " seconds";
+    cout << endl;
     return 0;
 }
